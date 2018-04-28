15355da43346385c8f12abb880bed94ff1981f47
cczapata <zapatacynthia@gmail.com>
2018-04-28 13:19:59 -0700

    added models, route

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+// const api_routes = require('./routes/api_routes');
+const port = process.env.PORT || 8080
+const app = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/AlphaStoreProducts');
+mongoose.Promise = Promise;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// api_routes(app);
+// require('./routes/api_routes')(app);
+app.get("/", function(req, res) {
+    console.log("hi");
+     res.send("hi");
+   });
+
+
+
+app.listen(port, () => console.log(`listening on ${port}`));
