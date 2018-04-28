5364171c4e20b0b1fb1f38729e71b4f9e22627c7
navidsarvian <navidsarv@gmail.com>
2018-04-28 11:06:04 -0700

    started modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,46 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav1 from "./components/Nav";
 import './App.css';
 import './index.css';
+import Modal from './components/Modal';
 
-const App = () =>
+class App extends Component {
+  state = {
+    show: false
+  }
+  showModal = () => {
+    this.setState({
+      ... this.state,
+      show: !this.state.show
+    });
+  }
+  render() {
+    return (
   <Router>
     <div>
       <Nav1 />
-      {/* <Switch>
-        <Route exact path="/" component={Books} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/books/:id" component={Detail} />
-        <Route component={NoMatch} />
-      </Switch> */}
+      
+      {/* <body onLoad={this.showModal}> */}
+      <input type="button" 
+      onClick={this.showModal} 
+      value="Show Modal" />
+
+      <Modal 
+      // onPageLoad={this.showModal}
+      onClose={this.showModal}
+      show={this.state.show}>
+        This message is from Modal!
+      </Modal> 
+      <Switch>
+        {/* <Route exact path="/" component={Books} /> */}
+        {/* <Route exact path="/modal" component={ageModal} /> */}
+        {/* <Route exact path="/books/:id" component={Detail} /> */}
+        {/* <Route component={NoMatch} /> */}
+      </Switch>
     </div>
-  </Router>;
+  </Router>
+    );
+  }
+}
 export default App;
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -28,7 +28,7 @@ const Nav1 = () =>
       {/* <a href="#brand"><img src="https://www.w3schools.com/images/w3schools_green.jpg"><img </a> */}
     </Navbar.Brand>
     <Navbar.Toggle />
-    <a href="localhost:3000"><img src="https://i.imgur.com/w2EwBqs.png?1" title="Gone Green Logo" width="128px" height="128px"/></a>
+    <a href="localhost:3000"><img src="https://i.imgur.com/w2EwBqs.png?1" title="Gone Green Logo" width="120px" height="120px" class="GGLogo"/></a>
 
   </Navbar.Header>
 
@@ -70,6 +70,9 @@ const Nav1 = () =>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
+
 </div>;
+
+
 
 export default Nav1;
