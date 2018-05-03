7ddd5313e79b6fa57d85ad4c401b4734b3bd808b
navidsarvian <navidsarv@gmail.com>
2018-05-02 20:11:06 -0700

    localhost /hi kinda works

--- client/src/App.js
+++ client/src/App.js
@@ -3,31 +3,36 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav1 from "./components/Nav";
 import './App.css';
 import './index.css';
-import Modal, { ModalContent } from './components/Modal';
+import Modal from './components/Modal';
+
+
+
+
 
 function MyPage(props) {
-  return <div>
-    hiiiiiiii
-  </div>;
+  return (
+  <div>
+    <img src="https://memegenerator.net/img/instances/73077992/no-weed-for-you.jpg" alt="no_weed"/>
+  </div>);
 }
 class App extends Component {
+
   state = {
-    show: true
+    show: true,
+    showNav: true
   }
+
   showModal = () => {
     this.setState({
-      ... this.state,
+      ...this.state,
       show: !this.state.show
     });
   }
-  renderNav() {
-    // if you don't want nav, ensure this conditional is true
-    if ('mycondition' === true) {   
-      return null;
-    }
 
-    // else return Nav
-    return <Nav1 />;
+  handleNav = () => {
+    this.setState({
+      showNav: false
+    });
   }
   
   render() {
@@ -35,7 +40,7 @@ class App extends Component {
   <Router>
     <div>
 
-      {this.renderNav()}
+      {!this.state.showNav ? null : <Nav1/>}
       
       {/* <body onLoad={this.showModal}> */}
       {/* <input type="button" 
@@ -43,8 +48,10 @@ class App extends Component {
       value="Show Modal" />
  */}
       <Modal 
-      onClose={this.showModal}
-      show={this.state.show}>
+        onClose={this.showModal}
+        show={this.state.show}
+        hansolo={this.handleNav}
+      >
       {/* <ModalContent /> */}
       {/* <img src="https://i.imgur.com/w2EwBqs.png?1" title="Gone Green Logo" width="95px" height="95px" class="GGLogo"/>
         
