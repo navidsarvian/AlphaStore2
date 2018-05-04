dfbd8139d6cd908b3b69c5bb85b44f3f16ab6ca2
unknown <romengistu@gmail.com>
2018-05-03 19:26:00 -0700

    Jumbotron & Products Component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Nav1 from "./components/Nav";
 import './App.css';
 import './index.css';
 import Modal, { ModalContent } from './components/Modal';
+import Jumbo from "./components/Jumbotron";
+import Products from "./components/ProductCards";
 
 function MyPage(props) {
   return <div>
@@ -22,36 +24,38 @@ class App extends Component {
   }
   renderNav() {
     // if you don't want nav, ensure this conditional is true
-    if ('mycondition' === true) {   
+    if ('mycondition' === true) {
       return null;
     }
 
     // else return Nav
     return <Nav1 />;
   }
-  
+
   render() {
     return (
   <Router>
     <div>
 
       {this.renderNav()}
-      
+
       {/* <body onLoad={this.showModal}> */}
-      {/* <input type="button" 
-      onClick={this.showModal} 
+      {/* <input type="button"
+      onClick={this.showModal}
       value="Show Modal" />
  */}
-      <Modal 
+ <Jumbo />
+ <Products />
+      <Modal
       onClose={this.showModal}
       show={this.state.show}>
       {/* <ModalContent /> */}
       {/* <img src="https://i.imgur.com/w2EwBqs.png?1" title="Gone Green Logo" width="95px" height="95px" class="GGLogo"/>
-        
-      
+
+
         Please Verify Your Age */}
       {/* </ModalContent> */}
-      </Modal> 
+      </Modal>
       <Switch>
         <Route exact path="/hi" component={MyPage} />
         {/* <Route exact path="/modal" component={ageModal} /> */}
