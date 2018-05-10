1a104658428862e274e3d7be4054ef925b75f165
lilmocc <lissa.prudencio@gmail.com>
2018-05-09 19:10:28 -0700

    navbar and robels changes

--- client/src/components/ProductCards/ProductCards.js
+++ client/src/components/ProductCards/ProductCards.js
@@ -8,9 +8,7 @@ import './ProductCards.css';
 const logo = '/IMG_0912.jpg';
 
 const Products = ({showProducts}) => {
-
   return (
-
     <Container style={{ marginTop: "35px" }}>
     <div class="page-header">
       <h1>Gone Green Goodies <small>Check out our full line of products</small></h1>
@@ -86,48 +84,7 @@ const Products = ({showProducts}) => {
         </CardDeck>
       </Row>
     </Container>
-
-      <Container-fluid style={{marginTop: "35px"}}>>
-      <Row>
-            <Col xs={12} md={12} className="mr-auto ml-auto">
-              <CardDeck>
-                <div className="custom-card">
-                  <Card>
-                    <CardImg top width="100%" src={logo} alt="Card image cap" />
-                    <CardBody>
-                      <CardTitle>Card title</CardTitle>
-                      <CardSubtitle>Card subtitle</CardSubtitle>
-                      <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                      <Button>Button</Button>
-                    </CardBody>
-                  </Card>
-                </div>
-                <div className="custom-card">
-                  <Card>
-                    <CardImg top width="100%" src={logo} alt="Card image cap" />
-                  <CardBody>
-                    <CardTitle>Card title</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
-                    <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-                    <Button>Button</Button>
-                  </CardBody>
-                </Card>
-              </div>
-              <div className="custom-card">
-                <Card>
-                  <CardImg top width="100%" src={logo} alt="Card image cap" />
-                  <CardBody>
-                    <CardTitle>Card title</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
-                    <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-                    <Button>Button</Button>
-                  </CardBody>
-                </Card>
-              </div>
-              </CardDeck>
-            </Col>
-            </Row>
-          </Container-fluid>  
+ 
 
   );
 };
