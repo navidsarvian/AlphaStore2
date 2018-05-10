d11156bbb96c55189596a2e7752eac61d3b07efc
navidsarvian <navidsarv@gmail.com>
2018-05-09 20:02:46 -0700

    made it so that when you click login page and then go back to home page it will keep all the products up

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,12 +4,6 @@ import { Form, FormGroup, Col } from 'reactstrap';
 export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.props.hideProducts();
-  }
-
-  componentDidMount() {
-    console.log('this is props inside products');
-    console.log(this.props);    
   }
 
   render (){
