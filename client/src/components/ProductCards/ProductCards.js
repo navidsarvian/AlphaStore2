7db404975bc96d539084e4efdcaadcd4148dcc8b
cczapata <zapatacynthia@gmail.com>
2018-05-09 21:33:42 -0700

    added products

--- client/src/components/ProductCards/ProductCards.js
+++ client/src/components/ProductCards/ProductCards.js
@@ -19,10 +19,14 @@ const Products = ({showProducts}) => {
             <Card>
               <CardImg top width="100%" src={logo} alt="Card image cap" />
               <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                <Button>Button</Button>
+                <CardTitle>Medi Blocks</CardTitle>
+                <CardSubtitle>Medicinal Benefits</CardSubtitle>
+                <CardText>
+                Our in house made concentrates are full spectrum and high in terpens
+                Death Bubba is known to help with a variety of symptoms including depression,
+                anxiety, appetite, nausea, pain, and sleep.
+                </CardText>
+                <Button>Add to Cart</Button>
               </CardBody>
             </Card>
           </div>
@@ -30,10 +34,12 @@ const Products = ({showProducts}) => {
           <Card>
             <CardImg top width="100%" src={logo} alt="Card image cap" />
             <CardBody>
-              <CardTitle>Card title</CardTitle>
-              <CardSubtitle>Card subtitle</CardSubtitle>
-              <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-              <Button>Button</Button>
+              <CardTitle>Medi Syrup</CardTitle>
+              <CardSubtitle>Medicinal Benefits</CardSubtitle>
+              <CardText>
+              Death Bubba is known to help with a variety of symptoms including depression, anxiety, appetite, nausea, pain, and sleep
+              </CardText>
+              <Button>Add to Cart</Button>
             </CardBody>
           </Card>
         </div>
@@ -41,10 +47,15 @@ const Products = ({showProducts}) => {
           <Card>
             <CardImg top width="100%" src={logo} alt="Card image cap" />
             <CardBody>
-              <CardTitle>Card title</CardTitle>
-              <CardSubtitle>Card subtitle</CardSubtitle>
-              <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-              <Button>Button</Button>
+              <CardTitle>Medi POP</CardTitle>
+              <CardSubtitle>Medicinal Benefits</CardSubtitle>
+              <CardText>
+              Our in house made concentrates are full spectrum and high in terpens
+Death Bubba is known to help with a variety of symptoms including depression, anxiety,
+appetite, nausea, pain, and sleep.175mg THC $10.00 or 300mg $20.00.
+              </CardText>
+              
+              <Button>Add to Cart</Button>
             </CardBody>
           </Card>
         </div>
