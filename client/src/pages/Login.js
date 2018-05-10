8dc9d287aa3c94a20a89f88139b2758ced2eccc9
lilmocc <lissa.prudencio@gmail.com>
2018-05-09 20:07:43 -0700

    login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { Form, FormGroup, Col } from 'reactstrap';
+// import { Form, FormGroup, Col } from 'reactstrap';
+import { Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import './Login.css';
 
 export default class Login extends Component {
   constructor(props) {
@@ -9,13 +11,32 @@ export default class Login extends Component {
 
   componentDidMount() {
     console.log('this is props inside products');
-    console.log(this.props);    
+    console.log(this.props);
   }
 
   render (){
     return (
-      <div>
-        <h1>Log In Page </h1>  
+      <div class="logincontainer">
+        <Form>
+       <FormGroup row>
+         <Label for="exampleEmail" sm={2}>Email</Label>
+         <Col sm={10}>
+           <Input type="email" name="email" id="exampleEmail" placeholder="mary@jane.com" />
+         </Col>
+       </FormGroup>
+       <FormGroup row>
+         <Label for="examplePassword" sm={2}>Password</Label>
+         <Col sm={10}>
+           <Input type="password" name="password" id="examplePassword" placeholder="enter password here" />
+         </Col>
+       </FormGroup>
+
+       <FormGroup check row>
+         <Col sm={{ size: 10 }}>
+           <Button color="success">Submit</Button>
+         </Col>
+       </FormGroup>
+     </Form>
       </div>
     )
   }
